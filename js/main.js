5363b78ba607de7b1950c13a638e02c10faca486
igniciousgaborone <fbda24-069@thuto.bac.ac.bw>
2025-06-04 13:49:22 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-// Mobile menu toggle
 const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
 const nav = document.querySelector('nav');
 
 mobileMenuBtn.addEventListener('click', () => {
   nav.classList.toggle('active');
 });
+
 
 // Close mobile menu when clicking a link
 const navLinks = document.querySelectorAll('nav a');
